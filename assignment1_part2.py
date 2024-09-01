2d58ad2be94d2c3a60e87919dae9e65400f0aad8
BrandonJuarezCunySPS <brandon.2100@live.com>
2024-09-01 16:32:02 -0400

    Add files via upload

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,23 @@
 
 class Book:
+    author = ''
+    title = ''
+
     def __init__(self, author, title):
-        pass
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
+        print(self.title + ", written by " + self.author)
 
 
-if __name__ == "__main__":
-    pass
+object1 = Book('J.K. Rowling', 'Harry Potter and the Goblet of Fire')
+object2 = Book('Walter Scott', 'Ivanhoe:A Romance')
+
+object1.display()
+print()
+object2.display()
+
+
+
+
